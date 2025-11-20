54012e216ecf09f4ab3faf6e9da8f99909d69e4d
wendel211 <wendelmuniz04@gmail.com>
2025-11-20 13:50:55 -0300

    feat(routes): add routing with protected routes for dashboard, users, and explore pages

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,48 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import Users from "../pages/Users";
+import Explore from "../pages/Explore";
+import NotFound from "../pages/NotFound";
+import { ProtectedRoute } from "../components/layout/ProtectedRoute";
+
+export function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/users"
+          element={
+            <ProtectedRoute>
+              <Users />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/explore"
+          element={
+            <ProtectedRoute>
+              <Explore />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
